b1b32b55f7fdbe4161431eff52aa1d68097d8bf8
Bongi-03 <bongiwe2003@gmail.com>
2025-03-11 10:12:39 +0200

    intro into python

---
+++ Python Navigation/Intro2.py
@@ -0,0 +1,20 @@
+# #integer (Whle num)
+# age = 21
+# #float (Decimals)
+# height = 4.9
+# #string (text)
+# name = "Bongi"
+# #boolean (True/False)
+# is_student = True
+
+# #Check data types
+# print(type(age)) #Output: <class: 'int'>
+# print(type(height)) #Output: <class: 'int'>
+# print(type(name)) #Output: <class: 'int'>
+# print(type(is_student)) #Output: <class: 'int'>
+
+#Dynamic Typing:
+age = 20
+print(age) #output : 20
+age = "Now I am twenty"
+print(age) #output : string
