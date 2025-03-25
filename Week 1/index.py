da8ece6b1ecf9619e3d94c5feab4c1ec2255f8d5
Bongi-03 <bongiwe2003@gmail.com>
2025-03-25 10:51:59 +0200

    Add calculator script and enhance index.py with variable examples

--- Week 1/index.py
+++ Week 1/index.py
@@ -1,1 +1,23 @@
 print("Hello World")
+
+number = 10
+print(number)
+
+
+
+
+
+age= 20 # Int Datatype
+marks = 90.5 # Float Datatype
+
+name = "Jane Doe" # String Datatype
+
+
+
+
+
+# Lists Datatypes
+
+languages = ["Python", "Java", "C++", "C#", 3, 4.5, True, False]
+#print(languages)
+print(languages[-6])
