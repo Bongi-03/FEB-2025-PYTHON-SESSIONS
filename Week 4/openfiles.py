f0db82801983cd1c7076f84da16afd586783dc80
Bongi-03 <bongiwe2003@gmail.com>
2025-04-01 10:15:07 +0200

    week 4 Python session

---
+++ Week 4/openfiles.py
@@ -0,0 +1,8 @@
+#Open a file in read mode
+#with open('Week 4/example.txt', 'r') as file:
+#    content = file.read()
+#   print(content)
+
+
+with open("Week 4/output.txt", "w") as file: 
+        file.write("Hello, Python!")
